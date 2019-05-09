03c1f8afa8e035bacec273b98e3b43400b601353
Johan Norberg <norberg_johan@hotmail.com>
2019-05-08 21:06:47 -0500

    Move implementation of Nes to cpp-file

--- application/src/nes.cpp
+++ application/src/nes.cpp
@@ -1,1 +1,44 @@
-#include "Nes.h"
+#include "nes.h"
+
+#include "core/cpu_factory.h"
+#include "core/membank_factory.h"
+#include "core/mmu_factory.h"
+#include "core/ppu_factory.h"
+#include "core/rom_factory.h"
+
+Nes::Nes()
+        : ppu_(n_e_s::core::PpuFactory::create(&ppu_registers_)),
+          mmu_(n_e_s::core::MmuFactory::create(
+                  n_e_s::core::MemBankFactory::create_nes_mem_banks(
+                          ppu_.get()))),
+          cpu_(n_e_s::core::CpuFactory::create(&cpu_registers_, mmu_.get())) {}
+
+void Nes::execute() {
+    if (cycle_++ % 3 == 0) {
+        cpu_->execute();
+    }
+
+    ppu_->execute();
+}
+
+void Nes::reset() {
+    cpu_->reset();
+}
+
+void Nes::load_rom(const std::string &filepath) {
+    n_e_s::core::MemBankList membanks{
+            n_e_s::core::MemBankFactory::create_nes_mem_banks(ppu_.get())};
+
+    std::unique_ptr<n_e_s::core::IRom> rom{
+            n_e_s::core::RomFactory::from_file(filepath)};
+    membanks.push_back(std::move(rom));
+
+    mmu_ = n_e_s::core::MmuFactory::create(std::move(membanks));
+    cpu_ = n_e_s::core::CpuFactory::create(&cpu_registers_, mmu_.get());
+
+    reset();
+}
+
+uint64_t Nes::cycle() const {
+    return cycle_;
+}
--- application/src/nes.h
+++ application/src/nes.h
@@ -1,56 +1,24 @@
 #pragma once
 
-#include "core/cpu_factory.h"
 #include "core/icpu.h"
 #include "core/immu.h"
 #include "core/ippu.h"
 #include "core/irom.h"
-#include "core/membank_factory.h"
-#include "core/mmu_factory.h"
-#include "core/ppu_factory.h"
-#include "core/rom_factory.h"
 
 #include <memory>
+#include <string>
 
 class Nes {
 public:
-    Nes()
-            : ppu_(n_e_s::core::PpuFactory::create(&ppu_registers_)),
-              mmu_(n_e_s::core::MmuFactory::create(
-                      n_e_s::core::MemBankFactory::create_nes_mem_banks(
-                              ppu_.get()))),
-              cpu_(n_e_s::core::CpuFactory::create(&cpu_registers_,
-                      mmu_.get())) {}
+    Nes();
 
-    void execute() {
-        if (cycle_++ % 3 == 0) {
-            cpu_->execute();
-        }
+    void execute();
 
-        ppu_->execute();
-    }
+    void reset();
 
-    void reset() {
-        cpu_->reset();
-    }
+    void load_rom(const std::string &filepath);
 
-    void load_rom(const std::string &filepath) {
-        n_e_s::core::MemBankList membanks{
-                n_e_s::core::MemBankFactory::create_nes_mem_banks(ppu_.get())};
-
-        std::unique_ptr<n_e_s::core::IRom> rom{
-                n_e_s::core::RomFactory::from_file(filepath)};
-        membanks.push_back(std::move(rom));
-
-        mmu_ = n_e_s::core::MmuFactory::create(std::move(membanks));
-        cpu_ = n_e_s::core::CpuFactory::create(&cpu_registers_, mmu_.get());
-
-        reset();
-    }
-
-    uint64_t cycle() const {
-        return cycle_;
-    }
+    uint64_t cycle() const;
 
     n_e_s::core::IPpu::Registers ppu_registers_{};
     std::unique_ptr<n_e_s::core::IPpu> ppu_;
